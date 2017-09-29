5c4d3677c070080206a6292f86edabbbdd9350ec
Dave Barner <dbarner@gmail.com>
2017-09-28 21:14:28 -0400

    added-basic-auth

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -1,6 +1,7 @@
 class CopiesController < ApplicationController
   before_action :set_copy, only: [:show, :edit, :update, :destroy]
 
+  http_basic_authenticate_with :name => "dbarner", :password => "Willow86", :only=> :destroy, :except => [:index, :show]
   # GET /copies
   # GET /copies.json
   def index
